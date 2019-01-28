6193da77960816c376e9373ad08ab2849c975f54
rockycse21 <rocky.pab1971@gmail.com>
2019-01-28 13:02:46 +0600

    third change

---
+++ README.md
@@ -0,0 +1,1 @@
+This is just html file with some image slider
---
+++ bootstrap/js/active.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".test").nerveSlider({
+        slideTransitionSpeed: 1000,
+        slideTransitionEasing: "easeOutExpo",
+        slideTransition: "fade",
+        sliderWidth: "100%",
+        sliderHeight: "375px"
+    });
+});
